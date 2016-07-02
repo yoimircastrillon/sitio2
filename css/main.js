98aa51ca36685d1a8709a3bd8ae0921e15a9cd7c
yoimircastrillon <yycastrillon@gmail.com>
2016-07-02 12:41:12 -0500

    sitio

---
+++ css/main.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+
+});
